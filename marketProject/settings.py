2812197902c6226dddb73edef3665ab328ae1814
alfatih <muhammadalfatihwibowo8@gmail.com>
2026-02-18 13:54:49 +0700

    add models: items and categories

--- marketProject/settings.py
+++ marketProject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'marketApp'
+    'marketApp',
+    'item'
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
